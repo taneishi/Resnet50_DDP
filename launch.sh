772707ce1fb5954c3870f40edde65d0e7637c97e
taneishi <taneishi@users.noreply.github.com>
2020-08-31 23:03:12 -0700

    fixed dataset length, merge two launch scripts.

--- launch.sh
+++
@@ -1,3 +0,0 @@
-#PBS -l nodes=4:ppn=2
-cd $PBS_O_WORKDIR
-mpirun  -machinefile $PBS_NODEFILE ./resnet_ddp.sh
--- resnet_ddp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /opt/intel/inteloneapi/pytorch/latest/bin/activate
-python resnet_ddp.py --local_rank=${PMI_RANK} --world_size=${PMI_SIZE}
